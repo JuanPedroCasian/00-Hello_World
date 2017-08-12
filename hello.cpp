d00d3b1bf472491e55a85c320a2d10392a926949
JuanPedroCasian <30848384+JuanPedroCasian@users.noreply.github.com>
2017-08-11 19:49:54 -0500

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,20 @@
+#include <iostream>      /*used to include the content of the package
+                          *(iostream stands for input output stream)
+                          *
+                          */
+
+using namespace std;     /*namespace is kind of a last name for the different
+                          *comands that you can use
+                          *
+                          * in this document cout is the comand inside the
+                          * namespace std and the namespace is inside the
+                          * <iostream>
+                          */
+
+int main () {                   //
+
+cout << "Hola mundo" << endl;
+
+return 0;
+
+}
